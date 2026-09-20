fdda9b8437aa51837c662a62c6da853ca5f720ea
pouria-nayeb-aboulghasem <PouriaNayebAb@gmail.com>
2026-09-20 02:33:08 -0700

    update search

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,29 @@ import MovieList from "@/components/MovieList";
 import Search from "@/components/Search";
 import { getMovies } from "@/services";
 import type { MovieType } from "@/types";
-import NotFound from "./components/NotFound";
+import NotFound from "@/components/NotFound";
+import { textFormat } from "@/utils";
 
 const App = () => {
   const [movies, setMovies] = useState<MovieType[]>(getMovies());
 
+  const filterSearch = (value: string): void => {
+    if (value === "") {
+      setMovies(getMovies());
+
+      return;
+    }
+
+    const filteredMovies = getMovies().filter((m) =>
+      textFormat(m.title).includes(textFormat(value)),
+    );
+
+    setMovies(filteredMovies);
+  };
+
   return (
     <>
-      {/* step 1: create search component */}
-      <Search setMovies={setMovies} />
+      <Search filterSearch={filterSearch} />
       {movies.length === 0 ? <NotFound /> : <MovieList movies={movies} />}
     </>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,40 +1,18 @@
-import type { MovieType } from "@/types";
 import { useState } from "react";
-import { getMovies } from "@/services";
-import { textFormat } from "@/utils";
 
 // step 3: define component props type
 type SearchProps = {
-  setMovies: (movies: MovieType[]) => void;
+  filterSearch: (value: string) => void;
 };
 
-// step 2: feed search component props
-const Search = ({ setMovies }: SearchProps) => {
-  // step 5: define search component state
+const Search = ({ filterSearch }: SearchProps) => {
   const [query, setQuery] = useState<string>("");
 
-  function resetMovies(value: string): void {
-    if (value === "") {
-      setMovies(getMovies());
-      setQuery("");
-
-      return;
-    }
-  }
-
   function handleSearch(value: string) {
-    resetMovies(value);
-
+    filterSearch(value);
     setQuery(value);
-
-    const filteredMovies = getMovies().filter((m) =>
-      textFormat(m.title).includes(textFormat(value)),
-    );
-
-    setMovies(filteredMovies);
   }
 
-  // step 4: define UI
   return (
     <div className="search">
       <label className="search__label" htmlFor="movie-search">
